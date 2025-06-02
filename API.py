853e560153002598525854409e80f76e70f4dfc0
lee <jimmy9999kimo@gmail.com>
2025-06-03 06:44:59 +0800

    404

--- API.py
+++ API.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import threading
 import dropbox
+import os
 
 app = Flask(__name__)
 
@@ -399,4 +400,5 @@ def get_status():
     return jsonify(status)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
